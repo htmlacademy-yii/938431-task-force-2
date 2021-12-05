11c1dfb6a039f0b22f83ad9f92e127f8c48d78b2
Yulia Elagina <julia-elagina@yandex.ru>
2021-12-06 00:46:29 +0300

    Реализует методы класса для конвертации csv файла в файл sql

--- src/models/CSVFileConverter.php
+++ src/models/CSVFileConverter.php
@@ -7,10 +7,7 @@ class CSVFileConverter {
     private $filename;
     private $tableName;
     private $columns;
-
     private $fileObject;
-    private $tableValues;
-    private $sqlFileName;
 
     public function __construct(string $filename, string $tableName, array $columns)
     {
@@ -19,24 +16,73 @@ class CSVFileConverter {
         $this->columns = $columns;
     }
 
-//        Создает массив из значений ячеек, записывает его в $tableValues
-    public function import(): void {
+    //  Записывает sql запросы в файл с переданным именем
+    public function writeSql(string $sqlFileName): void {
+        if (!$this->validateColumns($this->columns)) {
+            throw new FileFormatException("Заданы неверные заголовки стобцов");
+        }
+
+        if (!file_exists($this->filename)) {
+            throw new SourceFileException("Файл c полученным именем не существует");
+        }
+
+        try {
+            $this->fileObject = new \SplFileObject($this->filename, 'r');
+        }
+        catch (\RuntimeException $ex) {
+            throw new SourceFileException("Не удалось открыть файл для чтения");
+        }
+
+        $header_data = $this->getHeaderData();
+        if ($header_data !== $this->columns) {
+            throw new FileFormatException("Исходный файл не содержит необходимых столбцов");
+        }
+
+        try {
+            $sqlFileObject = new \SplFileObject($sqlFileName, "w");
+            foreach ($this->getNextLine() as $line) {
+                $query = $this->createSqlQuery($line);
+                $sqlFileObject->fwrite($query);
+            }
+        }
+        catch (\RuntimeException $ex) {
+            throw new SourceFileException("Не удалось открыть файл для записи");
+        }
     }
 
-//        Создает файл sql с именем таблицы, записывает путь к нему в $sqlFileName
-    public function write(): void {
+    //   Возвращает заголовки столбцов таблицы
+    private function getHeaderData(): ?array {
+        $this->fileObject->rewind();
+        return $this->fileObject->getcsv();
     }
 
-//    Возвращает путь к sql файлу с запросами на запись данных
-    public function getSQLFile(): ?string {
-        return $this->sqlFileName;
+    //   Возвращает массив значений очередной строки таблицы
+    private function getNextLine(): ?iterable {
+        $result = null;
+        while (!$this->fileObject->eof()) {
+            yield $this->fileObject->fgetcsv();
+        }
+        return $result;
+    }
+    //  Создает запрос на запись в таблицу на основании одной строки данных
+    private function createSqlQuery(array $line): string {
+        $columns = implode(',', $this->columns);
+        $values = implode(',', $line);
+        return "INSERT INTO " . $this->tableName . " (" . $columns . ") VALUES(" . $values . ");" . PHP_EOL;
     }
 
-//    Возвращает заголовки столбцов таблицы
-    private function getHeaderData(): ?array {}
-
-//    Возвращает массив значений очередной строки таблицы
-    private function getNextLine(): ?array {}
-
-    private function validateColumns(array $columns): bool {}
+    private function validateColumns(array $columns): bool {
+        $result = true;
+        if (count($columns)) {
+            foreach ($columns as $column) {
+                if (!is_string($column)) {
+                    $result = false;
+                    break;
+                }
+            }
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
 }
